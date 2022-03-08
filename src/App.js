c702c8976c513daad2a073585ae79dc689e57c84
bia-pequeno <bia.2017pequeno@gmail.com>
2022-03-08 16:55:30 -0300

    concluido

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import './App.css'
+class App extends Component {
+  state = {
+    tarefa: "",
+    lista: []
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  add = () => {
+    let {lista, tarefa} = this.state
+    if(tarefa != ""){
+      this.setState({
+        tarefa: "",
+        lista: lista.concat({
+          tarefa: tarefa,
+          id:Date.now()
+        })
+      })
+    }
+  }
+
+  remove = (id) => {
+    let{lista} = this.state
+    this.setState({
+      lista: lista.filter((item)=> {
+        return item.id != id
+      })
+    })
+  }
+  handleChange = (ev) => {
+    this.setState({
+      tarefa: ev.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1>ToDo List</h1>
+        <input onChange={this.handleChange} type="text" value={this.state.tarefa} />
+        <button onClick={this.add}>add</button>
+        <ul>
+          {this.state.lista.map((item) => (
+            <li>{item.tarefa} <button onClick={()=> {this.remove(item.id)}}> X </button></li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
